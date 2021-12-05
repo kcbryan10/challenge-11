57cb5a62d90f0cd54e98dd55c8bc249ab3f07855
kcbryan <kcbryan10@gmail.com>
2021-12-05 17:18:30 -0600

    add routes folder/file with basic setup/ added functionality to the server.js file

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require("express");
 const fs = require("fs")
 const PORT = process.env || 3000;
 const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ urlencoded: true}));
+
+require("./routes/route")(app);
+
+app.listen(PORT, function() {
+    console.log("app is live on PORT:" + PORT);
+})

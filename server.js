a6a604af4b9424350c7f5130c9570a9c2430829b
kcbryan <kcbryan10@gmail.com>
2021-12-05 19:08:46 -0600

    got rid of routes.js file (server.js will be used instead)/wrote app.gets/ createNote function

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const fs = require("fs")
-const PORT = process.env || 3000;
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname));
-app.use(express.urlencoded({ urlencoded: true}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}))
 
-require("./routes/route")(app);
+app.get("/api/notes", (req,res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 app.listen(PORT, function() {
     console.log("app is live on PORT:" + PORT);
 })
+
+function createNote (body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray= [];
+
+    if(notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+    );
+}
